f3d3f270fa708dff58a7b333cc987f7c0247b40d
TECH-GamerLOL <swordflameknight@gmail.com>
2025-03-10 19:50:53 +0800

    Created the player class and reseted main.

--- core/player.py
+++ core/player.py
@@ -1,5 +1,27 @@
 import math
 
 class Menkey ():
-    def __init__(start_x, start_y):
-        print("Testing still")
+    def __init__(self, start_x, start_y):
+        if not isinstance(start_x,(int, float)) or not isinstance(start_y(int, float)):
+            print("Must be num")
+            return
+        if start_x < 0 or start_y < 0:
+            print("Can't be negative")
+            return
+        
+        self.position = [start_y, start_x]
+        self.health = 3
+        self.speed = 2
+        self.power = []
+        self.invincible = False
+
+        def moveLeft(self):
+            self.position[0] -= self.speed
+        
+        def move_right(self):
+            self.position[0] += self.speed
+
+        def jump(self):
+            self.position[1] += self.speed
+
+        
