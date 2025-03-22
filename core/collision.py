4220cfb54a01d08bab96bb5c648e5742d2fe0231
TECH-GamerLOL <swordflameknight@gmail.com>
2025-03-22 10:55:59 +0800

    Created a collision.py file instead of doing it in main.py.

---
+++ core/collision.py
@@ -0,0 +1,43 @@
+class GameObject:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def get_rect(self):
+        return (self.x, self.y, self.width, self.height)
+
+def check_collision(obj1, obj2):
+    rect1 = obj1.get_rect()
+    rect2 = obj2.get_rect()
+
+    if (rect1[0] < rect2[0] + rect2[2] and
+        rect1[0] + rect1[2] > rect2[0] and
+        rect1[1] < rect2[1] + rect2[3] and
+        rect1[1] + rect1[3] > rect2[1]):
+        return True
+    return False
+
+class Game:
+    def __init__(self):
+        self.player = GameObject(50, 50, 50, 50)
+        self.enemy = GameObject(100, 100, 50, 50)
+        self.lives = 3
+        self.game_over = False
+
+    def update(self):
+        if check_collision(self.player, self.enemy):
+            self.lives -= 1
+            if self.lives <= 0:
+                self.game_over = True
+                print("Game Over")
+
+# Example usage
+game = Game()
+while not game.game_over:
+    game.update()
+    # Update player and enemy positions here
+    # For example:
+    # game.player.x += 1
+    # game.enemy.x -= 1
